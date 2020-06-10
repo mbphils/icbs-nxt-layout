b76762d113f5f87725fdc9028085b138b2d1de72
Carlos Alipio D. Rivera <cadrivera@gmail.com>
2020-06-10 12:02:51 +0800

    Customer Domains Initial
    Customer Domains for Layout

---
+++ grails-app/domain/icbs/customer/Customer.groovy
@@ -0,0 +1,52 @@
+package icbs.customer
+import icbs.admin.Branch
+import icbs.admin.UserMaster
+import icbs.lov.CustomerStatus
+import icbs.lov.CustomerType
+
+class Customer {
+    
+    CustomerType customerType
+    String cid
+    String displayName
+    String name1  // last name or institution name
+    String name2  // first name
+    String name3  // middle name
+    Date birthDate  // can also be registration date for institution
+    String birthPlace
+    byte[] customerPhoto
+    byte[] customerSignature
+    
+    Branch branch
+    UserMaster createdByUser
+    UserMaster lastUpdatedByUser
+    
+    CustomerStatus status
+    
+    static constraints = {
+        customerType nullable:true
+        cid nullable:true
+        displayName nullable:true
+        name1  nullable:true
+        name2  nullable:true
+        name3  nullable:true
+        birthDate  nullable:true
+        birthPlace nullable:true
+        customerPhoto nullable:true
+        customerSignature nullable:true
+    
+        branch nullable:true
+        createdByUser nullable:true
+        lastUpdatedByUser nullable:true
+    
+        status nullable:true        
+    }
+    static mapping = {
+    	id sqlType:'int', generator:'increment'
+    }
+    
+    String toString() {       
+        return cid + ' - ' + displayName
+    }
+    
+}
